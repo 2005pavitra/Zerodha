83e2a49f75380be4108b263d621e6247c92b87d6
Pavitra Pandey <pavitrapandeysagar@gmail.com>
2025-11-06 16:05:55 +0530

    Initial Setup- frotend + backend+ db

---
+++ backend/index.js
@@ -0,0 +1,22 @@
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import connectedDB from "./db.js";
+
+dotenv.config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+connectedDB();
+
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+    res.send('Backend is running')
+})
+
+app.listen(PORT,()=>{
+    console.log(`Server is running on port ${PORT}`);
+})
---
+++ frontend/src/App.jsx
@@ -0,0 +1,18 @@
+
+
+
+
+function App() {
+  
+
+  return (
+    <>
+     <div className='text-4xl font-bold text-center text-indigo-600'>
+      Zerodha Clone
+     </div>
+        
+    </>
+  )
+}
+
+export default App
